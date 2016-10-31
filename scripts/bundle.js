2d2838b2ee2b5ab8d53d6ff6991cfb5dfed39bb1
zackluckyf <fanning.34@gmail.com>
2016-10-30 22:24:33 -0400

    first typescript commit

---
+++ scripts/bundle.js
@@ -0,0 +1,54 @@
+var Car = (function () {
+    function Car(engine) {
+        this.engine = engine;
+    }
+    Car.prototype.start = function () {
+        console.log("Engine started " + this.engine);
+    };
+    Car.prototype.stop = function () {
+        console.log("Engine stopped " + this.engine);
+    };
+    return Car;
+}());
+window.onload = function () {
+    var car = new Car('V8');
+    car.start();
+    car.stop();
+};
+var num = 2;
+var num1 = 20;
+var num2 = 100;
+var lastName = 'smith';
+function addNumbers(n1, n2, n3) {
+    var result = n1 + n2 + n3;
+    console.log(result);
+}
+addNumbers(num, num1, num2);
+document.title = 'hello';
+var data = 'Hello John';
+$('div').text(data);
+var squareIt;
+var rectA = { h: 3 };
+var rectB = { h: 3, w: 4 };
+squareIt = function (rect) {
+    if (rect.w !== undefined) {
+        return rect.h * rect.w;
+    }
+    return rect.h * rect.h;
+};
+console.log(squareIt(rectA));
+console.log(squareIt(rectB));
+var p = {
+    name: 'Zack',
+    age: 23,
+    pets: 2,
+    calcPets: function () { return 2 * p.pets; },
+    greet: function (msg) {
+        return msg + ' ' + p.name;
+    }
+};
+var pets = p.calcPets();
+console.log(pets);
+var greeting = p.greet('hello');
+console.log(greeting);
+//# sourceMappingURL=bundle.js.map
---
+++ scripts/typescript.ts
@@ -0,0 +1,82 @@
+// hello world type bit
+class Car {
+  engine: string;
+  constructor(engine: string){
+    this.engine = engine;
+  }
+  start() {
+    console.log(`Engine started ${this.engine}`)
+  }
+  stop(){
+    console.log(`Engine stopped ${this.engine}`)
+  }
+}
+
+window.onload = function(){
+  var car = new Car('V8');
+  car.start();
+  car.stop();
+}
+
+// type anotations
+var num: number = 2;
+var num1 = 20;
+var num2: number = 100;
+var lastName: string = 'smith';
+
+function addNumbers(n1: number,n2: number,n3: number){
+  var result = n1+n2+n3;
+  console.log(result);
+}
+
+addNumbers(num,num1,num2);
+
+/// <reference path="node_modules/@types/jquery/index.d.ts" />
+
+document.title = 'hello';
+
+var data = 'Hello John';
+
+$('div').text(data);
+
+var squareIt:
+  (rect: {h: number, w?: number;}) => number;
+
+var rectA = {h: 3};
+var rectB = {h: 3, w:4};
+
+squareIt = function(rect){
+  if(rect.w !== undefined){
+    return rect.h * rect.w;
+  }
+  return rect.h * rect.h;
+}
+
+console.log(squareIt(rectA));
+console.log(squareIt(rectB));
+
+interface Person {
+  name : string;
+  age?: number;
+  pets: number;
+  calcPets: () => number;
+  greet: (msg: string) => string;
+}
+
+// since I used arrow functions with the methods
+// I had to use p instead of this since this is
+// lexically scoped to the window
+var p: Person = {
+  name : 'Zack',
+  age: 23,
+  pets: 2,
+  calcPets: () => 2*p.pets,
+  greet: (msg: string) => {
+    return msg + ' ' + p.name;
+  }
+};
+
+var pets = p.calcPets();
+console.log(pets);
+var greeting = p.greet('hello')
+console.log(greeting);
